dc48af72d985159788ee9057baa067c37686797f
jorge.fernandez@kapa7.com <jorge.fernandez@kapa7.com>
2017-05-14 16:26:42 -0600

    Add project files.

---
+++ Implementacion_de_patrones_G4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementacion_de_patrones_G4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
