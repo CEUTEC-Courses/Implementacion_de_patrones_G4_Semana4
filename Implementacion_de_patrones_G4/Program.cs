7798f8ca97a6a1256e7a6d5eadc093b676fb624e
gabri <gabri@GT>
2017-05-16 19:53:51 -0600

    Patron Singleton

--- Implementacion_de_patrones_G4/Program.cs
+++ Implementacion_de_patrones_G4/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Implementacion_de_patrones_G4.Builder;
 using Implementacion_de_patrones_G4.Prototype;
+using System.Threading;
+using Implementacion_de_patrones_G4.Singleton;
 
 namespace Implementacion_de_patrones_G4
 {
@@ -55,6 +57,30 @@ namespace Implementacion_de_patrones_G4
             Console.WriteLine("\n--- Fin de Prototype ---\n");
 
             /* Fin de Prototype  */
+
+            /*Prueba Singleton*/
+            Console.WriteLine("--- Pruebas Singleton ---\n");
+            
+            SingletonPatron s = new SingletonPatron();
+            Console.WriteLine(string.Format("Instancia {0} creada a las {1}",
+            s.Nombre, s.HoraArranque.ToLongTimeString()));
+
+            Console.WriteLine(string.Format("Espere mientras se intenta crear una segunda instancia "));
+           
+            // Hacemos una pausa de cinco segundos
+            Thread.Sleep(5000);
+
+            // Intentamos instanciar un segundo Singleton
+            SingletonPatron s2 = new SingletonPatron();
+
+            // Comprobamos que ambos objetos son referencias a la misma instancia, que es única
+            Console.WriteLine("\n");
+            Console.WriteLine(string.Format("Instancia {0} creada a las {1}",
+            s2.Nombre, s2.HoraArranque.ToLongTimeString()));
+            Console.WriteLine(string.Format("Comprobamos por la hora, que la instancia sigue siendo la misma"));
+            Console.WriteLine("\n--- Fin de Singleton ---\n");
+
+            /* Fin de Singleton  */
 
             Console.ReadLine();
         }
---
+++ Implementacion_de_patrones_G4/Singleton/SingletonPatron.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Implementacion_de_patrones_G4.Singleton
+{
+   class SingletonPatron
+    {
+        // Declaramos un atributo del mismo tipo de la clase con carácter estático
+        public static SingletonPatron _instancia = null;
+
+        public string Nombre { get; set; }
+        public DateTime HoraArranque { get; set; }
+
+        // Constructor privado. Únicamente puede ser invocado desde el interior
+        // de la propia clase
+        public SingletonPatron()
+        {
+            Nombre = "Patrón Singleton";
+            HoraArranque = DateTime.Now;
+        }
+
+        // Property de solo lectura
+        public static SingletonPatron Instance
+        {
+            get
+            {
+                // Si el singleton no ha sido creado previamente, se instancia.
+                // En caso contrario, se devolvera el que haya sido creado previamente
+                if (_instancia == null)
+                    _instancia = new SingletonPatron();
+
+                // Se devuelve la instancia
+                return _instancia;
+            }
+        }
+    }
+}
